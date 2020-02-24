adede7295ce0140916240e596a128f6634a518e7
Andrea Gallo <a_blade99@hotmail.com>
2020-02-24 17:54:37 +0100

    Section 2 first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.components.html',
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent {
+  title = 'This is a server item component'
+}
