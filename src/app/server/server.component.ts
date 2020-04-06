e5d1d3e6e19532b6ca029928c3de481a1732f737
andrea.gallo <andrea.gallo@deltatre.it>
2020-04-06 09:15:39 +0200

    Minor changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,10 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-server",
   templateUrl: "./server.component.html",
-  styleUrls: ["./server.component.scss"]
+  styleUrls: ["./server.component.scss"],
 })
 export class ServerComponent {
   title = "Server component";
   serverId: number = 10;
   serverStatus: string = "offline";
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
